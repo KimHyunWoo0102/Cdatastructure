ca7772668a1456ccc124b2911037e19f7c2b6332
KimHyunWoo0102 <rlagusdn60@gmail.com>
2025-05-24 15:50:26 +0900

    feat : LinkedList ADT 구현

---
+++ LinkedList/LinkedList.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#define FALSE 0
+#define TRUE 1
+#define MAX_SIZE 100
+
+//typedef int LData;
+typedef int LData;
+
+//List가 호환되었으면함.
+//main에서는 List 만 가지고 연산을 진행한다.
+//헤더내에서 List를 어떤걸 쓸지 정해줄수 있음
+
+typedef struct _node {
+	LData data;
+	struct _node* next;
+}Node;
+
+typedef struct _linkedList {
+	Node* head;
+	Node* cur;
+	Node* prev;
+	int numOfData;
+	int (*comp)(LData d1, LData d2);
+}LinkedList;
+
+typedef LinkedList List;
+
+int isEmpty(List* plist);
+
+void ListInit(List* plist);
+void LInsert(List* plist, LData data);
+int LFirst(List* plist, LData* pdata);
+int LNext(List* plist, LData* pdata);
+LData LRemove(List* plist);//직전 참조한 데이터 삭제
+int LCount(List* plist);
+void SetSortRule(List* plist, int(*comp)(LData d1, LData d2));
