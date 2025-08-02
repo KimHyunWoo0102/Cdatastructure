3391851dfd94969d7ce1d7171efb4591dec80f4c
KimHyunWoo0102 <rlagusdn60@gmail.com>
2025-08-02 20:32:48 +0900

    Implement : LinkedList base Queue

---
+++ Queue/LinkedQueue/main.c
@@ -0,0 +1,16 @@
+#include"ListBaseQueue.h"
+#include<stdio.h>
+
+int main() {
+	Queue queue;
+
+	QueueInit(&queue);
+
+	Enqueue(&queue, 1); Enqueue(&queue, 2);
+	Enqueue(&queue, 3); Enqueue(&queue, 4);
+	Enqueue(&queue, 5);
+
+	while (!QIsEmpty(&queue)) {
+		printf("%d ", Dequeue(&queue));
+	}
+}
